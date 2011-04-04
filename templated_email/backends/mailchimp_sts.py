3e41e7065800bb9fd38a3ef908fbb1442547ad0e
Bradley Whittington <radbrad182@gmail.com>
2011-04-04 17:32:50 +0200

    Adds (untested) support for mailchimp STS. Uses a fork of mailsnake from https://github.com/bradwhittington/mailsnake/

---
+++ templated_email/backends/mailchimp_sts.py
@@ -0,0 +1,25 @@
+import django_vanilla
+from mailsnake import MailSnakeSTS
+from django.conf import settings
+
+class TemplateBackend(django_vanilla.TemplateBackend):
+    def __init__(self, *args, **kwargs):
+        self.connection = MailSnakeSTS(apikey = settings.MAILCHIMP_API_KEY)
+        
+    def send(self, template_name, from_email, recipient_list, context, fail_silently=False):
+        config = getattr(settings,'TEMPLATED_EMAIL_MAILCHIMP',{}).get(template_name,{})
+        parts = self._render_email(template_name, context)
+        params={
+            'message':{
+                'subject':config.get('subject',_('%s email subject' % template_name)) % context,
+                'html':parts.get('html',''),
+                'text':parts.get('plain',''),
+                'from_name':' '.join(from_email.split(' ')[:-1]) or 'Nobody',
+                'from_email':from_email,
+                'to_email':recipient_list,
+            }
+            'track_opens':config.get('track_opens',False),
+            'track_clicks':config.get('track_clicks',False),
+            'tags':config.get('tags',[]),
+        }
+        self.connection.SendEmail(params)
