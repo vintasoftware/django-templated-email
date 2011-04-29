b4b8af85ff3a863b5efe2abd9cd5c474548f6c76
Bradley Whittington <radbrad182@gmail.com>
2011-04-29 22:16:43 +0200

    Again, dumb typo

--- templated_email/__init__.py
+++ templated_email/__init__.py
@@ -34,4 +34,4 @@ def send_templated_mail(template_name, from_email, recipient_list, context, fail
     See BackendClass.send.__doc__
     """ 
     connection = connection or get_connection()
-    return connection.send(template_name, from_email, recipient_list, context, fail_silently, headers=header)
+    return connection.send(template_name, from_email, recipient_list, context, fail_silently, headers=headers)
