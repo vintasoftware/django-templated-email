f4e661b1da6f5e7940584a3194d085064c847c3f
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2012-12-10 01:35:52 +0100

    Add basic testing infrastructure

---
+++ testsettings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+INSTALLED_APPS = (
+    'templated_email',
+)
+
+SECRET_KEY = "notimportant"
