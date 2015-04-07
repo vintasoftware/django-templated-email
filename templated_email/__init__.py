58efac2f38f596029c2dfb9f5084e1d75a8bf6b1
Jesse Denardo <denaje@gmail.com>
2015-04-07 09:10:24 -0400

    Remove RemovedInDjango19Warning regarding importlib

--- templated_email/__init__.py
+++ templated_email/__init__.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.utils.importlib import import_module
+from importlib import import_module
 from django.core.exceptions import ImproperlyConfigured
 from templated_email.backends.vanilla_django import TemplateBackend
 
