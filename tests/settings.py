492f488a5a8991af468bea9ced5407939d4b3d0a
Patrick Cloke <clokep@patrick.cloke.us>
2016-06-01 14:11:54 -0700

    Add simple test for rendering blocks.

--- tests/settings.py
+++ tests/settings.py
@@ -5,6 +5,22 @@ DATABASES = {
     }
 }
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': ['tests/templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages'
+            ],
+        },
+    },
+]
+
 INSTALLED_APPS = (
     'templated_email',
 )
