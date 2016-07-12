f994fb31ec545e39d28bf9e62b78ef87ce3838cd
André Ericson <de.ericson@gmail.com>
2016-07-11 21:05:47 -0300

    Add python 3 to setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ CLASSIFIERS = [
     'Intended Audience :: Developers',
     'License :: OSI Approved :: MIT License',
     'Operating System :: OS Independent',
-    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3',
     'Topic :: Software Development :: Libraries :: Python Modules',
     'Framework :: Django',
 ]
