7ad8d480d35d05f256671fd6d1cad66ec98b6325
André Ericson <de.ericson@gmail.com>
2016-09-01 16:08:25 -0300

    Updating setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,28 @@
+import os
+import sys
 from setuptools import setup
 
 DESCRIPTION = "A Django oriented templated / transaction email abstraction"
-
+VERSION = '0.5'
 LONG_DESCRIPTION = None
 try:
     LONG_DESCRIPTION = open('README.rst').read()
 except:
     pass
+
+requirements = [
+    'django-render-block>=0.5',
+    'six>=1',
+]
+
+# python setup.py register
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist upload")
+    args = {'version': VERSION}
+    print("You probably want to also tag the version now:")
+    print("  git tag -a %(version)s -m 'version %(version)s'" % args)
+    print("  git push --tags")
+    sys.exit()
 
 CLASSIFIERS = [
     'Development Status :: 5 - Production/Stable',
@@ -31,8 +47,5 @@ setup(
     long_description=LONG_DESCRIPTION,
     platforms=['any'],
     classifiers=CLASSIFIERS,
-    install_requires=[
-        'django-render-block',
-        'six',
-    ],
+    install_requires=requirements,
 )
