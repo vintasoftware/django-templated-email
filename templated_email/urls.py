b9d8f7a5f1efd8d4a4e9dd001b8249a2aa2c8e03
Sean Van <zfaner@gmail.com>
2017-02-07 10:20:39 +0800

    Email link 'show_email' urlpatterns regular rule update

--- templated_email/urls.py
+++ templated_email/urls.py
@@ -4,5 +4,5 @@ from templated_email.views import ShowEmailView
 
 app_name = 'templated_email'
 urlpatterns = [
-    url(r'^email/(?P<uuid>.*)/$', ShowEmailView.as_view(), name='show_email'),
+    url(r'^email/(?P<uuid>[0-9a-f]{32})/$', ShowEmailView.as_view(), name='show_email'),
 ]
