26d66b1e436abbd084101354720e3064ffe20f30
André Ericson <de.ericson@gmail.com>
2017-05-03 17:55:16 -0300

    Bump version: 2.1.0 → 2.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 from setuptools import setup, find_packages
 
 DESCRIPTION = "A Django oriented templated / transaction email abstraction"
-VERSION = '2.1.0'
+VERSION = '2.2.0'
 LONG_DESCRIPTION = None
 try:
     LONG_DESCRIPTION = open('README.rst').read()
