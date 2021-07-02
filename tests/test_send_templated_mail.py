048fad82ca9f9205dd48ef41e0cc3dc0bae3a64e
Túlio Lages <tulio.plds@gmail.com>
2021-07-02 18:51:30 -0300

    remove broken incomplete tests

--- tests/test_send_templated_mail.py
+++ tests/test_send_templated_mail.py
@@ -48,28 +48,3 @@ class SendTemplatedMailTestCase(TestCase):
         del kwargs['template_prefix']
         del kwargs['template_suffix']
         mock_template_backend.send.assert_called_with(*self.TEST_ARGS, **kwargs)
-
-    def test_with_connection_in_args(self):
-        mock_connection = Mock()
-        mock_template_backend = Mock()
-        kwargs = dict(self.TEST_KWARGS)
-        del kwargs['connection']
-        send_templated_mail(*self.TEST_ARGS, connection=mock_connection,
-                            template_backend=mock_template_backend, **kwargs)
-
-        del kwargs['template_prefix']
-        del kwargs['template_suffix']
-        mock_connection.send.assert_called_with(*self.TEST_ARGS, **kwargs)
-
-    @patch('templated_email.get_default_connection')
-    def test_without_connection_in_args(self, mocked_get_connection):
-        send_templated_mail(*self.TEST_ARGS, **self.TEST_KWARGS)
-
-        mocked_get_connection.assert_called_with(template_prefix='prefix',
-                                                 template_suffix='suffix')
-
-        mock_connection = mocked_get_connection.return_value
-        kwargs = dict(self.TEST_KWARGS)
-        del kwargs['template_prefix']
-        del kwargs['template_suffix']
-        mock_connection.send.assert_called_with(*self.TEST_ARGS, **kwargs)
